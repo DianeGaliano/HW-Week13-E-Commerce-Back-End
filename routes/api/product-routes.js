6a33881ea7ff951c65909a0e4f2ec7de603e7958
Diane Galiano <dianegaliano09@gmail.com>
2021-10-24 00:31:48 -0500

    working on fixing product-routs code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,14 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  
-
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category, Tag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, Tag}],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!"});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
