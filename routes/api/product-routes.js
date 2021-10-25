c1ca032d4343fdacf18c1eddf702440aaf8d6adb
Diane Galiano <dianegaliano09@gmail.com>
2021-10-25 13:24:58 -0500

    fixed some errors in the product-routes file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,12 +47,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body, {
+  Product.create ({
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
     tagIds: req.body.category_id,
-  }
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
